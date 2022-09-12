fa432cc320cdee2d4dfd560f0e3be91d473fa9ab
Eduardo Fayan <viniciusfayan@gmail.com>
2022-09-11 21:42:17 -0300

    commit-form-e-menu

---
+++ js/headerscript.js
@@ -0,0 +1,11 @@
+
+    function menuShow() {
+        let menuMobile = document.querySelector('.mobile-menu');
+        if (menuMobile.classList.contains('open')) {
+            menuMobile.classList.remove('open');
+            document.querySelector('.icon').src = "assets/fundopi.png";
+        } else {
+            menuMobile.classList.add('open');
+            document.querySelector('.icon').src = "assets/fundopi.png";
+        }
+    }
