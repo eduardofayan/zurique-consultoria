ce621f8770bb6aa7adcff49c60c6871c592274d0
Eduardo Fayan <viniciusfayan@gmail.com>
2022-09-29 08:39:46 -0300

    composer-install-phpmailer+ajustes-no-contato

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8e776a5cc0a1511ecc07f9fc6842a68fb565f177',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8e776a5cc0a1511ecc07f9fc6842a68fb565f177',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.6.4',
+      'version' => '6.6.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a94fdebaea6bd17f51be0c2373ab80d3d681269b',
+    ),
+  ),
+);
