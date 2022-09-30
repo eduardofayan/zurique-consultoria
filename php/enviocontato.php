ae2d2a88b99c4ea42495a30faf9c8fe91b4e4605
Eduardo Fayan <viniciusfayan@gmail.com>
2022-09-30 13:30:30 -0300

    envio-form-com-redirect

--- php/enviocontato.php
+++ php/enviocontato.php
@@ -1,60 +1,43 @@
 <?php
+ 
+  //Variáveis
+  $nomecontato = addslashes($_POST['nomecontato']);
+  $telcontato = addslashes($_POST['telcontato']);
+  $emailcontato = addslashes($_POST['emailcontato']);
+  $empresacontato = addslashes($_POST['empresacontato']);
+  $mensagemcontato = addslashes($_POST['contentcontato']);
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
 
+  //Compo E-mail
+  $arquivo = "
+    Formúlario de Solicitar Contato
+      Nome: $nomecontato
+      Telefone para contato: $telcontato
+      E-mail: $emailcontato
+      Nome da Empresa: $empresacontato
+      Mensagem: $mensagemcontato
+      Este e-mail foi enviado em $data_envio às $hora_envio
+  ";
+  
+  //Emails para quem será enviado o formulário
+  $destino = "viniciusfayan@gmail.com";
+  $assunto = "Solicitação de Contato";
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
 
-
-require 'vendor/autoload.php';
-
-if(isset($_POST['enviar'])){
-
-//Create an instance; passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'developereduardofayan@gmail.com';                     //SMTP username
-    $mail->Password   = 'vkmwgwovvyscmroj';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;          //Enable implicit TLS encryption
-    $mail->Port       = '465';                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-
-    //Recipients
-    $mail->setFrom('developereduardofayan@gmail.com', 'Email');
-    $mail->addAddress('viniciusfayan@gmail.com', 'Solicitação de Contato');     //Add a recipien   
-    //$mail->addReplyTo('info@example.com', 'Information');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
-
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Solicitação de Contato';
-    $body = "Informações de Contato: <br>
-    Nome: ". isset($_POST['nomecontato'])." <br>
-    Telefone para contato: ". isset($_POST['telcontato']) ." <br>
-    Email: ". isset($_POST['emailcontato'])." </br>
-    Nome da empresa: ". isset($_POST['empresacontato'])." <br>
-    Mensagem: <br>
-    ". isset($_POST['contentcontato']);
-
-    $mail->Body    = $body;
-
-    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-    $mail->send();
-    echo 'Mensagem enviada com sucesso, em breve entraremos em contato!';
-} catch (Exception $e) {
-    echo "Ocorreu um erro no envio, tentar novamente: {$mail->ErrorInfo}";
-}
-}else{
-  echo "Ocorreu um erro no envio, tentar novamente: {$mail->ErrorInfo}";
-}
+ //Enviar
+ if (mail($destino, $assunto, $arquivo, $headers)){
+  echo ("Email Enviado com sucesso! Em breve Entraremos em contato! <meta http-equiv='refresh' content='10;URL=../demonstracao.html'> ") ;
+} else {
+  echo ("Ocorreu um erro no envio, estamos te redirecionando para tentar Novamente");
+};
+if($_POST):
+  if(isset($_POST['url'])&&strlen($_POST['url'])==0 ) {
+      echo "Validado Redirecionando...";
+  }
+endif;
+?>
