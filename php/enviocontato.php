9bbe58479d2934110c523add5fb73f1adb50b088
Eduardo Fayan <viniciusfayan@gmail.com>
2022-10-03 15:39:45 -0300

    envio-form-finalizado

--- php/enviocontato.php
+++ php/enviocontato.php
@@ -1,17 +1,19 @@
 <?php
- 
+
+date_default_timezone_set('America/Sao_Paulo');  
+
   //Variáveis
-  $nomecontato = addslashes($_POST['nomecontato']);
-  $telcontato = addslashes($_POST['telcontato']);
-  $emailcontato = addslashes($_POST['emailcontato']);
-  $empresacontato = addslashes($_POST['empresacontato']);
-  $mensagemcontato = addslashes($_POST['contentcontato']);
+  $nomecontato = $_POST['nomecontato'];
+  $telcontato = $_POST['telcontato'];
+  $emailcontato = $_POST['emailcontato'];
+  $empresacontato = $_POST['empresacontato'];
+  $mensagemcontato = $_POST['contentcontato'];
   $data_envio = date('d/m/Y');
   $hora_envio = date('H:i:s');
 
-  //Compo E-mail
+  //Corpo E-mail
   $arquivo = "
-    Formúlario de Solicitar Contato
+    Formulário de Solicitar Contato
       Nome: $nomecontato
       Telefone para contato: $telcontato
       E-mail: $emailcontato
@@ -21,23 +23,18 @@
   ";
   
   //Emails para quem será enviado o formulário
-  $destino = "viniciusfayan@gmail.com";
+  $destino = "edivan@zurique.com.br";
   $assunto = "Solicitação de Contato";
 
   //Este sempre deverá existir para garantir a exibição correta dos caracteres
   $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
+  $headers .= "Content-type: text/html; charset=utf-8\n";
+  $headers .= "From: viniciusfayan@gmail.com";
 
  //Enviar
  if (mail($destino, $assunto, $arquivo, $headers)){
-  echo ("Email Enviado com sucesso! Em breve Entraremos em contato! <meta http-equiv='refresh' content='10;URL=../demonstracao.html'> ") ;
+  echo ("Email Enviado com sucesso! Em breve Entraremos em contato!") ;
 } else {
   echo ("Ocorreu um erro no envio, estamos te redirecionando para tentar Novamente");
 };
-if($_POST):
-  if(isset($_POST['url'])&&strlen($_POST['url'])==0 ) {
-      echo "Validado Redirecionando...";
-  }
-endif;
 ?>
--- php/enviodemonstracao.php
+++ php/enviodemonstracao.php
@@ -1,24 +1,25 @@
 <?php
- 
+  
+  date_default_timezone_set('America/Sao_Paulo');
+  
   //Variáveis
-  $nome = addslashes($_POST['nome']);
-  $sobrenome = addslashes($_POST['sobrenome']);
-  $email = addslashes($_POST['email']);
-  $tel = addslashes($_POST['tel']);
-  $empresa = addslashes($_POST['empresa']);
-  $mensagem = addslashes($_POST['content']);
+  $nomecontato = $_POST['nome'];
+  $sobrenomecontato = $_POST['sobrenome'];
+  $emailcontato = $_POST['email'];
+  $telcontato = $_POST['tel'];
+  $empresacontato = $_POST['empresa'];
+  $mensagemcontato = $_POST['content'];
   $data_envio = date('d/m/Y');
   $hora_envio = date('H:i:s');
 
-  //Compo E-mail
+  //Corpo E-mail
   $arquivo = "
-    Formúlario de Solicitar Demonstração
-      Nome: $nome
-      Sobrenome: $sobrenome
-      E-mail: $email
-      Telefone: $tel
-      Nome da Empresa: $empresa
-      Mensagem: $mensagem
+    Formulário de Solicitar de Demonstração
+      Nome: $nomecontato $sobrenomecontato
+      Telefone para contato: $telcontato
+      E-mail: $emailcontato
+      Nome da Empresa: $empresacontato
+      Mensagem: $mensagemcontato
       Este e-mail foi enviado em $data_envio às $hora_envio
   ";
   
@@ -28,18 +29,13 @@
 
   //Este sempre deverá existir para garantir a exibição correta dos caracteres
   $headers  = "MIME-Version: 1.0\n";
-  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
-  $headers .= "From: $nome <$email>";
+  $headers .= "Content-type: text/html; charset=utf-8\n";
+  $headers .= "From: viniciusfayan@gmail.com";
 
-  //Enviar
-  if (mail($destino, $assunto, $arquivo, $headers)){
-    echo ("Email Enviado com sucesso! Em breve Entraremos em contato! <meta http-equiv='refresh' content='10;URL=../demonstracao.html'> ") ;
-  } else {
-    echo ("Ocorreu um erro no envio, estamos te redirecionando para tentar Novamente");
-  };
-  if($_POST):
-    if(isset($_POST['url'])&&strlen($_POST['url'])==0 ) {
-        echo "Validado Redirecionando...";
-    }
-  endif;
+ //Enviar
+ if (mail($destino, $assunto, $arquivo, $headers)){
+  echo ("Email Enviado com sucesso! Em breve Entraremos em contato!") ;
+} else {
+  echo ("Ocorreu um erro no envio, estamos te redirecionando para tentar Novamente");
+};
 ?>
